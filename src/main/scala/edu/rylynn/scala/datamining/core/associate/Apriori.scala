03acfc42a8b7c4c43bb26d1b21985e368b15aede
rylynn-abo <rylynn_ab@163.com>
2018-05-11 10:46:33 +0800

    finish apriori in scala

--- src/main/scala/edu/rylynn/scala/datamining/core/associate/Apriori.scala
+++ src/main/scala/edu/rylynn/scala/datamining/core/associate/Apriori.scala
@@ -17,13 +17,6 @@ class Apriori(minSupport: Double, minConfidence: Double, data: List[String]) {
     count.toDouble / transactions.size.toDouble
   }
 
-  def generateSuperItemSet(cni: Set[String], cnj: Set[String]): Set[String] = {
-    val superSet = cni.++(cnj)
-    if (superSet.size == cni.size + 1)
-      superSet
-    else
-      null
-  }
 
   def string2Set(s: String): Set[String] = Set(s)
 
@@ -32,24 +25,67 @@ class Apriori(minSupport: Double, minConfidence: Double, data: List[String]) {
   }
 
   def generateFrequentItemSet: List[(Set[String], Double)] = {
+    def generateSuperItemSet(cni: Set[String], cnj: Set[String]): Set[String] = {
+
+      val superSet = cni.++(cnj)
+      if (superSet.size == cni.size + 1)
+        superSet
+      else
+        null
+    }
+
     val items: Set[String] = transactions.reduce((x1, x2) => x1 ++ x2)
 
-    val lastFrequentSet: List[(Set[String], Double)] = items.map(item => (string2Set(item), countSupport(string2Set(item)))).filter(_._2 >= minSupport).toList
+    var lastFrequentSet: List[(Set[String], Double)] = items.map(item => (string2Set(item), countSupport(string2Set(item)))).filter(_._2 >= minSupport).toList
     frequentItemSet = frequentItemSet ++ lastFrequentSet
+    var flag: Int = 1
+    while (flag == 1) {
+      var newItemSet: Set[Set[String]] = Set[Set[String]]()
 
-    //while (true) {
-      val superSet = lastFrequentSet.map(cni=>lastFrequentSet.map(cnj=>generateSuperItemSet(cni._1, cnj._1))).filter(superSet=>superSet!=null)
-      print(superSet)
+      val cni: List[Set[String]] = lastFrequentSet.map(_._1)
+      val cnj: List[Set[String]] = lastFrequentSet.map(_._1)
 
-    //}
+      cni.foreach(cnii => newItemSet = newItemSet ++ cnj.map(cnjj => generateSuperItemSet(cnii, cnjj)).filter(items => items != null))
+
+
+      val newFrequentSet: List[(Set[String], Double)] = newItemSet.toList.map(newItem => (newItem, countSupport(newItem))).filter(_._2 >= minSupport)
+      if (newFrequentSet.isEmpty) {
+        flag = 0
+      }
+      else {
+        frequentItemSet ++= newFrequentSet
+      }
+      lastFrequentSet = newFrequentSet
+    }
     frequentItemSet
+  }
+
+  def generateRules: List[(Set[String], Set[String], Double)] = {
+    generateFrequentItemSet
+
+    def subItemSet(itemSet: Set[String]): List[Set[String]] = itemSet.subsets().toList.filter(s => s.nonEmpty && s.size != itemSet.size)
+
+    def strongRules(A: Set[String], B: Set[String], supportAB: Double): (Set[String], Set[String], Double) = {
+      if (A.isEmpty || B.isEmpty)
+        null
+
+      val supportA = frequentItemSet.filter(s => s._1.equals(A)).head._2
+      val confidence: Double = supportAB / supportA
+      if (confidence > minConfidence)
+        (A, B, confidence)
+      else
+        null
+    }
+
+    frequentItemSet.flatMap(frequentSet => subItemSet(frequentSet._1).map(
+      subSet => strongRules(subSet, frequentSet._1.diff(subSet), frequentSet._2)).filter(r => r != null))
   }
 }
 
 object Test {
   def main(args: Array[String]): Unit = {
     val list = List("a,b,c,d", "c,d,e,f", "c,d,e,f", "c,d,e,f", "c,d,e,f")
-    val set = Set("a", "c")
-    new Apriori(0.3, 0.8, list).generateFrequentItemSet
+    //new Apriori(0.3, 0.8, list).generateFrequentItemSet.foreach(s=>println(s._1, s._2))
+    new Apriori(0.3, 0.8, list).generateRules.foreach(r=>println(r))
   }
 }
